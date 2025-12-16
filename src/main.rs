290cd62957bc5d591a3ccf2f061f9d3fc0684f6a
git-bruh <prathamIN@proton.me>
2025-12-16 21:57:08 +0530

    feat: resolve IP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use std::{
     io::{BufRead, BufReader, BufWriter, Write},
-    net::{Ipv4Addr, SocketAddrV4},
+    net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs},
     sync::Arc,
 };
 use tapstack::tun::TunDevice;
 use tracing::info;
 use tracing_subscriber::{fmt, layer::SubscriberExt, Registry};
+
+fn resolve(url: &str) -> std::net::SocketAddrV4 {
+    for addr in url.to_socket_addrs().unwrap() {
+        if let std::net::SocketAddr::V4(addr) = addr {
+            return addr;
+        }
+    }
+
+    panic!("no IPv4 address resolved");
+}
 
 fn main() {
     let subscriber = Registry::default().with(fmt::layer().with_writer(std::io::stderr));
@@ -18,33 +28,57 @@ fn main() {
         std::thread::spawn(move || dev.read_packets().unwrap())
     };
 
-    let socket = dev
-        .connect(SocketAddrV4::new(Ipv4Addr::new(78, 46, 170, 2), 80))
-        .unwrap();
+    if true {
+        let socket = dev.connect(resolve("tcpbin.com:4242")).unwrap();
 
-    let mut writer = BufWriter::new(&socket);
-    let mut reader = BufReader::new(&socket);
+        let mut writer = BufWriter::new(&socket);
+        let mut reader = BufReader::new(&socket);
 
-    let req = format!(
-        "{} {} {}\r\nHost: {}\r\nUser-Agent: {}\r\nAccept: {}\r\n\r\n",
-        "GET", "/100MB.bin", "HTTP/1.0", "fsn1-speed.hetzner.com", "curl/8.13.0", "*/*"
-    );
-    writer.write_all(req.as_bytes()).unwrap();
-    writer.flush().unwrap();
+        writer.write_all(b"hello").unwrap();
+        writer.flush().unwrap();
 
-    let mut buf = String::new();
-    let mut content_length = 0;
-    while buf != "\r\n" {
-        buf.clear();
+        socket.close();
+
+        let mut buf = String::new();
         reader.read_line(&mut buf).unwrap();
-        info!("Got header: {buf:?}");
-        if buf.starts_with("Content-Length:") {
-            content_length =
-                u64::from_str_radix(&buf["Content-Length: ".len()..buf.len() - 2], 10).unwrap();
-        }
-    }
 
-    socket.reset();
+        info!("read after close: {buf}");
+    } else {
+        let url = "fsn1-speed.hetzner.com:80";
+        let path = "/100MB.bin";
+
+        let socket = dev.connect(resolve(url)).unwrap();
+
+        let mut writer = BufWriter::new(&socket);
+        let mut reader = BufReader::new(&socket);
+
+        let req = format!(
+            "{} {} {}\r\nHost: {}\r\nUser-Agent: {}\r\nAccept: {}\r\n\r\n",
+            "GET",
+            path,
+            "HTTP/1.0",
+            url.split(':').nth(0).unwrap(),
+            "curl/8.13.0",
+            "*/*"
+        );
+        writer.write_all(req.as_bytes()).unwrap();
+        writer.flush().unwrap();
+
+        let mut buf = String::new();
+        let mut content_length = 0;
+        while buf != "\r\n" {
+            buf.clear();
+            reader.read_line(&mut buf).unwrap();
+            info!("Got header: {buf:?}");
+            if buf.starts_with("Content-Length:") {
+                content_length =
+                    u64::from_str_radix(&buf["Content-Length: ".len()..buf.len() - 2], 10).unwrap();
+            }
+        }
+
+        info!("Got content length: {content_length}");
+        socket.reset();
+    }
 
     reader_handle.join().expect("failed to join thread");
 }
